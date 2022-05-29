a6dacb308602b988855896247b5738a04820f5e7
desh610 <deshan.biss@gmail.com>
2022-05-29 17:27:46 +0530

    Added Layout appbar

--- lib/Components/Layouts/LayoutsShowcase.dart
+++ lib/Components/Layouts/LayoutsShowcase.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LayoutsShowcase extends StatelessWidget {
   const LayoutsShowcase({Key? key}) : super(key: key);
@@ -6,7 +7,24 @@ class LayoutsShowcase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: GestureDetector(
+          onTap: () {
+            Get.back();
+          },
+          child: Container(
+              child: Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.black,
+          )),
+        ),
+        title: Text(
+          'Layouts',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 1,
+      ),
     );
   }
 }
